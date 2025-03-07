d931b719a21f702c4fcd11f98aa93c18a2ad40eb
Jenish Bhandari <118617487+mejenish@users.noreply.github.com>
2025-03-07 12:43:03 +0545

    Committed to learning

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import Subfooter from './Subfooter.jsx'
+import React, { useState } from 'react'
 
 const Footer = () => {
+  const [newsletter, setNewsletter] = useState('')
+
+  const subMit = ((e)=>{
+    e.preventDefault();
+    console.log(newsletter);
+    setNewsletter('');
+  })
+
+
   return (
-    <div>
-      <h1>Welcome!</h1>
-      <Subfooter/>
+    <div className='w-full py-6 px-10 bg-emerald-600 mt-10'>
+      <h1 className='text-white text-2xl font-bold'>Welcome!</h1>
+      <input value={newsletter} onChange={(e)=>setNewsletter(e.target.value)}  className='outline-none bg-white px-3 py-3 rounded mr-1' type='text' placeholder='Enter you email'/>
+      <button onClick={e=> subMit(e)} className='bg-red-600 px-3 py-3 rounded'>Subscribe</button>
     </div>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,7 +7,7 @@ const Products = (props) => {
     margin: "0px 0px 0px 40px"
   }
   return (
-    <div className='inline-block h-120 w-65 py-6 px-3 bg-gray-100 ml-10 mt-10 rounded relative'>
+    <div className='inline-block h-120 w-65 py-6 px-3 bg-gray-100 ml-5 mt-10 rounded relative'>
       <img src={props.pPicture} alt='products' style={sStyle} />
       <h1 className='text-2xl font-medium mt-5'>{props.pModel}</h1>
       <h3>{props.pBrand} | {props.pName}</h3>
@@ -17,7 +17,7 @@ const Products = (props) => {
           return <li key={key}>{data}</li>
         })}</h4>
       </ol>
-      <h3 className='font-medium text-orange-600 absolute bottom-14'>Rs. {Math.round(props.pPrice)}</h3>
+      <h3 className='font-medium text-orange-600 absolute bottom-14'>${Math.round(props.pPrice)}</h3>
       <button className='text-white bg-emerald-600 px-3 py-2 rounded absolute bottom-2 center w-60'>Buy Now</button>
     </div>
   )
